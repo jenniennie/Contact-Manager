eb4c5636ebe80a382228df0247218a570313cff4
NJamison1 <84431644+NJamison1@users.noreply.github.com>
2022-02-10 14:55:09 -0500

    Fixed delete operation

--- LAMPAPI/DeleteContact.php
+++ LAMPAPI/DeleteContact.php
@@ -1,41 +1,26 @@
+
 <?php
+
 	$inData = getRequestInfo();
 
-	$userId = $inData["userId"];
-	$FirstName = $inData["FirstName"];
-	$LastName = $inData["LastName"];
-	$Email = $inData["Email"];
-	$Phone = $inData["Phone"];
-	$today = date("Y-m-d H:i:s"); 
-	$DateCreated = $today;
-
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error) 
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+	if( $conn->connect_error )
 	{
 		returnWithError( $conn->connect_error );
-	} 
-	else 
+	}
+	else
 	{
-		$sql = "DELETE FROM contacts where userId=$userId";
-		printf('Data deleted successfully');
-		
-	}	
+		$stmt = $conn->prepare("DELETE FROM ContactList WHERE ID=? AND UserId =?");
+		$stmt->bind_param("ii", $inData["ID"], $inData["UserId"]);
+		$stmt->execute();
 
+		$stmt->close();
+		$conn->close();
+	}
+	
 	function getRequestInfo()
 	{
 		return json_decode(file_get_contents('php://input'), true);
-	}
-
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
 	}
 	
 ?>
